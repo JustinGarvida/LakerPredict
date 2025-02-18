eab8a749c584a00c0fb174fa90039d51452b07a4
JustinGarvida <justingarvida1@gmail.com>
2025-02-18 14:51:13 -0500

    changed route names

--- backend/app/routes/player_routes.py
+++ backend/app/routes/player_routes.py
@@ -5,23 +5,23 @@ user_bp = Blueprint('user_bp', __name__)
 
 # Define routes
 @user_bp.route('/players', methods=['GET'])
-def get_users():
-    return jsonify({"message": "List of users"})
+def get_players():
+    return jsonify({"message": "List of players"})
 
 @user_bp.route('/players', methods=['POST'])
-def create_user():
+def create_player():
     data = request.get_json()
-    return jsonify({"message": "User created", "data": data}), 201
+    return jsonify({"message": "Player created", "data": data}), 201
 
-@user_bp.route('/players/<int:user_id>', methods=['GET'])
-def get_user(user_id):
-    return jsonify({"message": f"User with ID {user_id}"})
+@user_bp.route('/players/<string:player_id>', methods=['GET'])
+def get_player(player_id):
+    return jsonify({"message": f"Player with ID {player_id}"})
 
-@user_bp.route('/players/<int:user_id>', methods=['PUT'])
-def update_user(user_id):
+@user_bp.route('/players/<string:player_id>', methods=['PUT'])
+def update_player(player_id):
     data = request.get_json()
-    return jsonify({"message": f"User {user_id} updated", "data": data})
+    return jsonify({"message": f"Player {player_id} updated", "data": data})
 
-@user_bp.route('/players/<int:user_id>', methods=['DELETE'])
-def delete_user(user_id):
-    return jsonify({"message": f"User {user_id} deleted"})
+@user_bp.route('/players/<string:player_id>', methods=['DELETE'])
+def delete_player(player_id):
+    return jsonify({"message": f"Player {player_id} deleted"})
