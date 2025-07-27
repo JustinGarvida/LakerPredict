e7b569317f834a32f79aaea3e27f0e3c6df63f60
JustinGarvida <justingarvida1@gmail.com>
2025-07-27 19:14:50 -0400

    updating project struct

--- backend/app/routes/__init__.py
+++
@@ -1,6 +0,0 @@
-from flask import Blueprint
-from .player_routes import user_bp
-
-def register_blueprints(app):
-    app.register_blueprint(user_bp, url_prefix='/api')   # Prefixes all routes with /api
-    app.register_blueprint(product_bp, url_prefix='/api') # Prefixes all routes with /api
